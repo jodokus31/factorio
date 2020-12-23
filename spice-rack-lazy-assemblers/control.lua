81abfd2104a52dabf1e983fb7b3f06d22202e5fd
jodokus31 <jenspete@googlemail.com>
2020-12-23 17:38:19 +0100

    Recalculate Lazy Bastard Marathon according to changes in 1.1

--- spice-rack-lazy-assemblers/control.lua
+++ spice-rack-lazy-assemblers/control.lua
@@ -15,9 +15,15 @@ end)
 function on_player_creation(player, player_index)
     -- only first player gets it 
     if player_index == 1 then
-        if settingsutil.get_startup_setting("lazy-bastard-marathon") then
-            player.insert { name = "copper-cable", count = 96}
-            player.insert { name = "iron-gear-wheel", count = 48}
+        if game.difficulty_settings.recipe_difficulty == 1 then
+            if game.difficulty_settings.technology_price_multiplier == 4 then
+                if settingsutil.get_startup_setting("lazy-bastard-marathon") then
+                    player.insert { name = "copper-cable", count = 48}
+                    player.insert { name = "iron-gear-wheel", count = 14}
+                    -- player.insert { name = "iron-plate", count = 207}
+                    -- player.insert { name = "copper-plate", count = 47}
+                end
+            end
         end
     end
 end
