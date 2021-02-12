1c5c8f9483acfddecfbd849537df346cfbd68c79
jodokus31 <33641866+jodokus31@users.noreply.github.com>
2021-02-12 23:50:36 +0100

    Add lazy bastard marathon items to ship debris

--- spice-rack-lazy-assemblers/control.lua
+++ spice-rack-lazy-assemblers/control.lua
@@ -1,29 +1,15 @@
 local settingsutil = require('__spice-rack-core__/util/settingsutil')
 
-script.on_event(defines.events.on_player_created, function(event)
-    local player = game.players[event.player_index]
-    if player.character then
-        on_player_creation(player, event.player_index)
-    end
-end)
+script.on_init(function()
+    if game.difficulty_settings.recipe_difficulty ~= 1 then return end
+    if game.difficulty_settings.technology_price_multiplier ~= 4 then return end
 
-script.on_event(defines.events.on_cutscene_cancelled, function(event)
-    local player = game.players[event.player_index]
-    on_player_creation(player, event.player_index)
-end)
-
-function on_player_creation(player, player_index)
-    -- only first player gets it 
-    if player_index == 1 then
-        if game.difficulty_settings.recipe_difficulty == 1 then
-            if game.difficulty_settings.technology_price_multiplier == 4 then
-                if settingsutil.get_startup_setting("lazy-bastard-marathon") then
-                    player.insert { name = "copper-cable", count = 48}
-                    player.insert { name = "iron-gear-wheel", count = 14}
-                    -- player.insert { name = "iron-plate", count = 207}
-                    -- player.insert { name = "copper-plate", count = 47}
-                end
-            end
+    if settingsutil.get_startup_setting("lazy-bastard-marathon") then
+        if remote.interfaces["freeplay"] ~= nil then
+            debris_items = remote.call("freeplay", "get_debris_items")
+            debris_items["copper-cable"] = (debris_items["copper-cable"] or 0) + 48
+            debris_items["iron-gear-wheel"] = (debris_items["iron-gear-wheel"] or 0) + 14
+            remote.call("freeplay", "set_debris_items", debris_items)
         end
     end
-end
+end)
